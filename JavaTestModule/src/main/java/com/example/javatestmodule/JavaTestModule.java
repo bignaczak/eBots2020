7478343add907b53d6ceb0ae339142d793cf96e9
Brian Ignaczak <bignaczak@gmail.com>
2020-09-23 07:43:34 -0400

    Imported FTC SDK 9/13/20
    Added module JavaTestModule to enable java coding

---
+++ JavaTestModule/src/main/java/com/example/javatestmodule/JavaTestModule.java
@@ -0,0 +1,10 @@
+package com.example.javatestmodule;
+
+public class JavaTestModule {
+
+    public static void main(String args[]){
+        String myName = "Brian";
+        System.out.println("Hello " + myName + "!");
+
+    }
+}
